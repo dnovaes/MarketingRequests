4f55d6150b64fdfb3c4b584d7063be173dde5154
dnovaes <novaesdigo@gmail.com>
2019-04-11 00:53:09 -0300

    new Fragment created and binded with the GrahicPieceActivity - layout contains fade transition, button corner bordered, new font Roboto, constrains layout.

---
+++ app/src/main/java/com/example/marketingrequests/Fragments/GraphicPieceFragment.kt
@@ -0,0 +1,45 @@
+package com.example.marketingrequests
+
+import android.os.Bundle
+import android.os.Handler
+import android.transition.*
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.marketingrequests.databinding.FragmentTypegraphicpieceBinding
+
+
+class GraphicPieceFragment: Fragment(){
+
+    var scene1: Scene? = null
+    var scene2: Scene? = null
+    var scene3: Scene? = null
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        //return super.onCreateView(inflater, container, savedInstanceState)
+        val binding: FragmentTypegraphicpieceBinding = DataBindingUtil.inflate(inflater,
+            R.layout.fragment_typegraphicpiece, container, false)
+
+        return binding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        val rootGroup:ViewGroup = view!!.findViewById(R.id.graphicpieceFragment)
+        TransitionManager.beginDelayedTransition(rootGroup)
+
+        scene1 = Scene.getSceneForLayout(rootGroup , R.layout.scene1, activity)
+        //scene1?.enter()
+        scene2 = Scene.getSceneForLayout(rootGroup, R.layout.scene2, activity)
+        scene3 = Scene.getSceneForLayout(rootGroup, R.layout.fragment_typegraphicpiece, activity)
+
+        //https://developer.android.com/training/transitions#kotlin (by Code or Resource file)
+        //var fadeTransiction: Transition = Fade()
+        var fadeGroupTransiction: Transition = TransitionInflater.from(activity).inflateTransition(R.transition.set_bt_typegraphic)
+        TransitionManager.go(scene3, fadeGroupTransiction)
+    }
+}
+

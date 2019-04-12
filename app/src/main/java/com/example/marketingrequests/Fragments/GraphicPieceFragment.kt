598c2e1ddfbd06ce4cbe4a10b6d3ccf3086603a8
dnovaes <novaesdigo@gmail.com>
2019-04-11 23:34:14 -0300

    Add Navigation Component and new Fragment - Navigation.xml file corresponding the NavGraph of the project - New Fragment screen file: ListGraphicPiecesFragment. It will list all the items to be requested by the user. - Add click handlers in the buttons of GraphicPiecesActivitiy directing to the next fragment using Navigation component.

--- app/src/main/java/com/example/marketingrequests/Fragments/GraphicPieceFragment.kt
+++ app/src/main/java/com/example/marketingrequests/Fragments/GraphicPieceFragment.kt
@@ -1,34 +1,40 @@
 package com.example.marketingrequests
 
 import android.os.Bundle
-import android.os.Handler
 import android.transition.*
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.example.marketingrequests.databinding.FragmentTypegraphicpieceBinding
-
 
 class GraphicPieceFragment: Fragment(){
 
-    var scene1: Scene? = null
+    //private var scene1: Scene? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         //return super.onCreateView(inflater, container, savedInstanceState)
         val binding: FragmentTypegraphicpieceBinding = DataBindingUtil.inflate(inflater,
             R.layout.fragment_typegraphicpiece, container, false)
 
+        binding.btDigitalPiece.setOnClickListener(
+           //v.findNavController().navigate(R.id.action_graphicPieceFragment_to_listGraphicPiecesFragment)
+            Navigation.createNavigateOnClickListener(R.id.action_graphicPieceFragment_to_listGraphicPiecesFragment)
+        )
+
+        binding.btPrintedPiece.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_graphicPieceFragment_to_listGraphicPiecesFragment)
+        )
         return binding.root
     }
 
     override fun onResume() {
         super.onResume()
 
-        val rootGroup:ViewGroup = view!!.findViewById(R.id.graphicpieceFragment)
-        TransitionManager.beginDelayedTransition(rootGroup)
-
+        /*
+        val rootGroup:ViewGroup = view!!.findViewById(R.id.graphicPieceNavHostFragment)
         scene1 = Scene.getSceneForLayout(rootGroup , R.layout.fragment_typegraphicpiece, activity)
         //scene1?.enter()
 
@@ -36,6 +42,8 @@ class GraphicPieceFragment: Fragment(){
         //var fadeTransiction: Transition = Fade()
         var fadeGroupTransiction: Transition = TransitionInflater.from(activity).inflateTransition(R.transition.set_bt_typegraphic)
         TransitionManager.go(scene1, fadeGroupTransiction)
+        */
+
     }
 }
 

d5cce684cd5d7d08874b5cb7c46299adb33df71e
dnovaes <novaesdigo@gmail.com>
2019-04-13 10:33:16 -0300

    add code to receive the arguments by Bundle getArguments() in ListGraphicPiecesFragment

--- app/src/main/java/com/example/marketingrequests/Fragments/ListGraphicPiecesFragment.kt
+++ app/src/main/java/com/example/marketingrequests/Fragments/ListGraphicPiecesFragment.kt
@@ -4,14 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-
-//import com.example.marketingrequests.databinding.FragmentTypegraphicpieceBinding
-
 
 class ListGraphicPiecesFragment: Fragment(){
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        val args = arguments //receives a Bundle
+
+        Toast.makeText(context, "Title: ${args!!.getString("typeGraphicSelected")}", Toast.LENGTH_LONG).show()
+
         return super.onCreateView(inflater, container, savedInstanceState)
     }
 }

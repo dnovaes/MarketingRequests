e05e69efcf4b23c347a6b7bafd89a19a230719fb
dnovaes <novaesdigo@gmail.com>
2019-04-15 20:07:51 -0300

    add new fragment: list graphicpieces and yours string values - in this list of Graphical Pieces, the user is is supposed to choice what items will he choose. - initial layout

--- app/src/main/java/com/example/marketingrequests/Fragments/ListGraphicPiecesFragment.kt
+++ app/src/main/java/com/example/marketingrequests/Fragments/ListGraphicPiecesFragment.kt
@@ -5,23 +5,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.example.marketingrequests.databinding.FragmentListgraphicpiecesBinding
 
 class ListGraphicPiecesFragment: Fragment(){
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
+        val binding: FragmentListgraphicpiecesBinding = DataBindingUtil.inflate(inflater,
+            R.layout.fragment_listgraphicpieces, container, false)
+
         val args = arguments //receives a Bundle
 
         val toolbarTitle = args!!.getString("typeGraphicSelected")
-
-        Toast.makeText(context, "Title: ${toolbarTitle}", Toast.LENGTH_LONG).show()
+        Toast.makeText(context, "Title: ${toolbarTitle}", Toast.LENGTH_SHORT).show()
 
         activity!!.findViewById<TextView>(R.id.toolbarTitle).setText(toolbarTitle)
 
-        return super.onCreateView(inflater, container, savedInstanceState)
+        //return super.onCreateView(inflater, container, savedInstanceState)
+        return binding.root
     }
 }
 

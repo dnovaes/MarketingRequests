089cdc700a1d2a14e2860720e0de94977cd05437
dnovaes <novaesdigo@gmail.com>
2019-04-26 15:20:32 -0300

    add direct reference to string in first activity instead of changing it on runTime

--- app/src/main/java/com/example/marketingrequests/ui/Activities/MainActivity.kt
+++ app/src/main/java/com/example/marketingrequests/ui/Activities/MainActivity.kt
@@ -4,9 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.ViewModelProviders
 import com.example.marketingrequests.R
-import com.example.marketingrequests.viewmodel.GraphicPiecesViewModel
 import com.example.marketingrequests.databinding.ActivityMainBinding
 
 class MainActivity: AppCompatActivity(){
@@ -17,9 +15,6 @@ class MainActivity: AppCompatActivity(){
 
         //setContentView(R.layout.activity_main)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-
-        val model = ViewModelProviders.of(this).get(GraphicPiecesViewModel::class.java)
-        binding.title = model.getTitle()
 
         binding.btreqGraphicPiece.setOnClickListener{
            val intentReqGraphicPiece = Intent(this, GraphicPieceActivity::class.java)
